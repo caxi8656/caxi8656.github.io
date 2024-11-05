4816df233f3d3fa9a7173bd214c13b3d8ca6ad67
caxi8656 <94191884+caxi8656@users.noreply.github.com>
2024-11-05 10:23:40 -0700

    inclass11

---
+++ ica/ica11.js
@@ -0,0 +1,14 @@
+function tellFortune(childrenNo,partnerName,location,job)
+{
+    console.log("You will be a " + job + " in " + location + " , and married to " + partnerName + " with " + childrenNo + " kids.")
+}
+
+function calculateDogAge (puppyAge)
+{
+    console.log("Your doggie is " + puppyAge * 7 + " years old in dog years!")
+}
+
+function reverseNumber (someNo)
+{
+    console.log()
+}
