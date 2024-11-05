697c407c06f9ddefe0f12da8835f3f2629c66e61
caxi8656 <94191884+caxi8656@users.noreply.github.com>
2024-11-05 10:42:44 -0700

    made a whoopsie

--- ica/ica11.js
+++ ica/ica11.js
@@ -1,30 +1,31 @@
-function tellFortune(childrenNo,partnerName,location,job)
-{
-    console.log("You will be a " + job + " in " + location + " , and married to " + partnerName + " with " + childrenNo + " kids.");
+function tellFortune() {
+    let childrenNo = prompt("Enter the number of children:");
+    let partnerName = prompt("Enter the name of your partner:");
+    let location = prompt("Enter a location:");
+    let job = prompt("Enter a job title:");
+
+    console.log("You will be a " + job + " in " + location + ", and married to " + partnerName + " with " + childrenNo + " kids.");
 }
 
-function calculateDogAge(puppyAge)
-{
+function calculateDogAge() {
+    let puppyAge = prompt("Enter your dog's age:");
     console.log("Your doggie is " + puppyAge * 7 + " years old in dog years!");
 }
 
-function reverseNumber(numberArray)
-{
-    reverseArray = numberArray.reverse();
+function reverseNumber() {
+    let numberArray = prompt("Enter a series of numbers separated by commas:").split(",");
+    let reverseArray = numberArray.reverse();
     console.log(reverseArray);
 }
 
-function alphabetString(letterString)
-{
-    reverseLetter = letterString.split('').sort().join('');
-
-    console.log(reverseLetter);
+function alphabetString() {
+    let letterString = prompt("Enter a string:");
+    let sortedString = letterString.split('').sort().join('');
+    console.log(sortedString);
 }
 
-function upperConverter(lowerString)
-{
-    upperString = lowerString.charAt(0).toUpperCase() + str.slice(1);
+function upperConverter() {
+    let lowerString = prompt("Enter a string:");
+    let upperString = lowerString.charAt(0).toUpperCase() + lowerString.slice(1);
     console.log(upperString);
 }
-
-
