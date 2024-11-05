5b3284cb67994d664220557f3a94879c45557472
caxi8656 <94191884+caxi8656@users.noreply.github.com>
2024-11-05 10:49:50 -0700

    output

--- ica/ica11.js
+++ ica/ica11.js
@@ -4,28 +4,33 @@ function tellFortune() {
     let location = prompt("Enter a location:");
     let job = prompt("Enter a job title:");
 
-    console.log("You will be a " + job + " in " + location + ", and married to " + partnerName + " with " + childrenNo + " kids.");
+    let result = "You will be a " + job + " in " + location + ", and married to " + partnerName + " with " + childrenNo + " kids.";
+    document.getElementById("output").innerText = result;
 }
 
 function calculateDogAge() {
     let puppyAge = prompt("Enter your dog's age:");
-    console.log("Your doggie is " + puppyAge * 7 + " years old in dog years!");
+    let result = "Your doggie is " + (puppyAge * 7) + " years old in dog years!";
+    document.getElementById("output").innerText = result;
 }
 
 function reverseNumber() {
     let numberArray = prompt("Enter a series of numbers separated by commas:").split(",");
     let reverseArray = numberArray.reverse();
-    console.log(reverseArray);
+    let result = "Reversed numbers: " + reverseArray.join(", ");
+    document.getElementById("output").innerText = result;
 }
 
 function alphabetString() {
     let letterString = prompt("Enter a string:");
     let sortedString = letterString.split('').sort().join('');
-    console.log(sortedString);
+    let result = "Alphabetically sorted: " + sortedString;
+    document.getElementById("output").innerText = result;
 }
 
 function upperConverter() {
     let lowerString = prompt("Enter a string:");
     let upperString = lowerString.charAt(0).toUpperCase() + lowerString.slice(1);
-    console.log(upperString);
+    let result = "Converted string: " + upperString;
+    document.getElementById("output").innerText = result;
 }
