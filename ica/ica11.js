01377a04bf7acc9e05f8c26c93a35aefaaae3f2f
caxi8656 <94191884+caxi8656@users.noreply.github.com>
2024-11-05 10:41:57 -0700

    buttons

--- ica/ica11.js
+++ ica/ica11.js
@@ -1,14 +1,30 @@
 function tellFortune(childrenNo,partnerName,location,job)
 {
-    console.log("You will be a " + job + " in " + location + " , and married to " + partnerName + " with " + childrenNo + " kids.")
+    console.log("You will be a " + job + " in " + location + " , and married to " + partnerName + " with " + childrenNo + " kids.");
 }
 
-function calculateDogAge (puppyAge)
+function calculateDogAge(puppyAge)
 {
-    console.log("Your doggie is " + puppyAge * 7 + " years old in dog years!")
+    console.log("Your doggie is " + puppyAge * 7 + " years old in dog years!");
 }
 
-function reverseNumber (someNo)
+function reverseNumber(numberArray)
 {
-    console.log()
+    reverseArray = numberArray.reverse();
+    console.log(reverseArray);
 }
+
+function alphabetString(letterString)
+{
+    reverseLetter = letterString.split('').sort().join('');
+
+    console.log(reverseLetter);
+}
+
+function upperConverter(lowerString)
+{
+    upperString = lowerString.charAt(0).toUpperCase() + str.slice(1);
+    console.log(upperString);
+}
+
+
