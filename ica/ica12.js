b6208eb90fb477fb3063e67c1188843df0d03c14
caxi8656 <94191884+caxi8656@users.noreply.github.com>
2024-11-14 11:16:25 -0700

    api change

--- ica/ica12.js
+++ ica/ica12.js
@@ -1,7 +1,7 @@
 document.getElementById("jokeButton").addEventListener("click", fetchJoke);
 
 function fetchJoke() {
-    fetch("https://official-joke-api.appspot.com/jokes/random") // Replace this with your API URL
+    fetch("https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single") // Replace this with your API URL
         .then(response => response.json())
         .then(data => {
             // Assuming the API structure you provided, display the joke
